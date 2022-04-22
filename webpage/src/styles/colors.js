8fc968e0d842e0e03e2605420061d2a72c70c51e
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-04-22 14:47:31 -0300

    style(styles paste): add GlobalStyles and colors variables

---
+++ webpage/src/styles/colors.js
@@ -0,0 +1,20 @@
+export const layoutColors = {
+  primary: '#190632',
+  secondary: '#452D82',
+  tertiary: '#2D0B59',
+  highlight: '#FF9A0A',
+  text: '#FFF2E0',
+  layerBlur1: "#FE0ABA",
+  layerBlur2: "#930AFE",
+};
+
+export const tagColors = {
+  desafio: '#118884',
+  curso: '#FE0ABA',
+  htmlcss: '#930AFE',
+  javascript: '#FE700A',
+  banco: '#3B0AFE',
+  react: '#04C9C3',
+  node: '#0AFE6C',
+};
+
