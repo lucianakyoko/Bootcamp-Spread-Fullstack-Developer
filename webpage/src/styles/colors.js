5cf73d7f8f1e41c0d4e504f1effaaedacfe351af
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-04-22 16:19:59 -0300

    style: Add new colors

--- webpage/src/styles/colors.js
+++ webpage/src/styles/colors.js
@@ -3,9 +3,13 @@ export const layoutColors = {
   secondary: '#452D82',
   tertiary: '#2D0B59',
   highlight: '#FF9A0A',
+  highlightHover: '#EB6A0D',
+  logo: '#4B1196',
+  logoHover: '#57229C',
   text: '#FFF2E0',
   layerBlur1: "#FE0ABA",
   layerBlur2: "#930AFE",
+  shadow: "#170326",
 };
 
 export const tagColors = {
