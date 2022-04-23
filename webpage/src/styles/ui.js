ec1421f98acc4eea95519d5785a6fe515021a0b6
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-04-23 10:39:28 -0300

    style(ui.js): Create ui.js file

---
+++ webpage/src/styles/ui.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import { layoutColors } from "./colors";
+
+export const Title = styled.h1`
+  color: ${layoutColors.highlight};
+  font-size: 2rem;
+  font-weight: 700;
+  text-align: center;
+  text-transform: uppercase;
+`;
+
+export const ParagraphMedium = styled.p`
+  color: ${layoutColors.text};
+  font-size: 1.8rem;
+  font-weight: 500;
+  line-height: 30px;
+  text-align: left;
+`;
