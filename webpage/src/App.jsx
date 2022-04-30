8496f1b43787031aa7731ff26ce583605480e970
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-04-29 22:04:07 -0300

    feat(webpage/ About): Create new page component

--- webpage/src/App.jsx
+++ webpage/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import ProjectFull from "./pages/ProjectFull";
 import Projects from "./pages/Projects";
-
+import About from "./pages/About";
 import { Wrapper } from "./styles/app";
 import GlobalStyles from "./styles/GlobalStyles";
 
@@ -16,7 +16,8 @@ function App() {
       <main>
         {/* <Home /> */}
         {/* <Projects /> */}
-        <ProjectFull />
+        {/* <ProjectFull /> */}
+        <About />
       </main>
       <Footer />
     </Wrapper>
