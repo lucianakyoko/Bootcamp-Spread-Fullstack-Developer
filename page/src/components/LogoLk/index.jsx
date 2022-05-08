f660ed71c74757091a3be83f20ab2cf0fa2c3ed9
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-08 20:19:15 -0300

    style(page/components): add missing anchor atributes at LogoLk component

--- page/src/components/LogoLk/index.jsx
+++ page/src/components/LogoLk/index.jsx
@@ -1,7 +1,12 @@
 import { Logo } from './style';
 export function LogoLk() {
   return (
-    <Logo>
+    <Logo
+      href='https://lucianakyoko.com'
+      rel="noopener noreferrer" 
+      target="_blank"
+      title="lucianakyoko.com"
+    >
       LK
     </Logo>
   );
