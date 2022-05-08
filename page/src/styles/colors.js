70ba675944881b384c5965661cb35328e6eb01e3
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-08 20:11:49 -0300

    style(page/styles): add new color

--- page/src/styles/colors.js
+++ page/src/styles/colors.js
@@ -1,10 +1,12 @@
 export const layoutColors = {
   primary: '#190632',
   secondary: '#2D0B59',
-  tertiary: '#930AFE',
+  tertiary: '#4B1196',
+  tertiaryHover: '#930AFE',
   hightlight: '#FF9A0A',
   highlightHover: '#EB6A0D',
   light1: 'FE0ABA',
   light2: '930AFE',
   text: 'E6E3EB',
+  textShadow: '0px 2px 1px #FE0ABA;'
 }
