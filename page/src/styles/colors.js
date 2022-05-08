b219d9374147671a100e1a362e4c9ed05b867951
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-08 19:37:01 -0300

    style(page/styles): create layout color palette

---
+++ page/src/styles/colors.js
@@ -0,0 +1,10 @@
+export const layoutColors = {
+  primary: '#190632',
+  secondary: '#2D0B59',
+  tertiary: '#930AFE',
+  hightlight: '#FF9A0A',
+  highlightHover: '#EB6A0D',
+  light1: 'FE0ABA',
+  light2: '930AFE',
+  text: 'E6E3EB',
+}
