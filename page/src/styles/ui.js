3471594417c49e69d2a364fb5e3782f30cbe18be
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-09 18:38:05 -0300

    style(page/ui): add H2 styled component

--- page/src/styles/ui.js
+++ page/src/styles/ui.js
@@ -2,19 +2,23 @@ import styled from 'styled-components';
 import { layoutColors } from "./colors";
 
 export const headerSize = {
-  navHightMin: '8rem',
-  navHightMax: '12rem',
+  navHightMin: '4rem',
+  navHightMax: '6rem',
 };
 
 export const H1 = styled.h1`
   color: ${layoutColors.hightlight};
-  font-size: 2rem;
-  font-weight: bold;
+  font-size: 1.8rem;
+  font-weight: 700;
   text-align: center;
   text-transform: uppercase;
 
   @media screen and (min-width: 700px) {
-    font-size: 3.2rem;
+    font-size: 2.4rem;
   }
 `
-
+export const H2 = styled.h2`
+  color: ${layoutColors.hightlight};
+  font-size: 2rem;
+  font-weight: 700;
+`
