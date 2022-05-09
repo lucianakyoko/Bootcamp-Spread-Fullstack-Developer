c4dd6eb66e77fc5720cfd7a41545591c60e8370f
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-09 18:39:51 -0300

    style(page/app): fixing top position

--- page/src/styles/app.js
+++ page/src/styles/app.js
@@ -6,9 +6,10 @@ export const MainWrapper = styled.main`
   height: 100vh;
 
   position: relative;
-  top: calc(${headerSize.navHightMin} + 2.4rem);
+  top: ${headerSize.navHightMin};
+  z-index: -1;
   
   @media screen and (min-width: 700px) {
-    top: calc(${headerSize.navHightMax} + 2.4rem);
+    top: ${headerSize.navHightMax};
   }
 `
