a0574164128cfbdde8d72fe9b7dae64e666020f3
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-09 15:23:43 -0300

    style(page/ui): create H1 styled component

--- page/src/styles/ui.js
+++ page/src/styles/ui.js
@@ -1,6 +1,20 @@
+import styled from 'styled-components';
 import { layoutColors } from "./colors";
 
 export const headerSize = {
   navHightMin: '8rem',
   navHightMax: '12rem',
-}
+};
+
+export const H1 = styled.h1`
+  color: ${layoutColors.hightlight};
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  text-transform: uppercase;
+
+  @media screen and (min-width: 700px) {
+    font-size: 3.2rem;
+  }
+`
+
