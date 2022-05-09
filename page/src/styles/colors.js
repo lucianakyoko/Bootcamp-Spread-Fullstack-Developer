771bdde31f75579d0968fddadae0280590e97338
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-09 13:51:26 -0300

    style(page/styles): fixing colors declaration

--- page/src/styles/colors.js
+++ page/src/styles/colors.js
@@ -5,8 +5,8 @@ export const layoutColors = {
   tertiaryHover: '#930AFE',
   hightlight: '#FF9A0A',
   highlightHover: '#EB6A0D',
-  light1: 'FE0ABA',
-  light2: '930AFE',
-  text: 'E6E3EB',
+  light1: '#FE0ABA',
+  light2: '#930AFE',
+  text: '#E6E3EB',
   textShadow: '0px 2px 1px #FE0ABA;'
 }
