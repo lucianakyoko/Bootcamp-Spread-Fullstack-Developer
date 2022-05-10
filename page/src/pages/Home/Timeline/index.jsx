6cd701a1bf7c2756a383401d0c6c07374a4d2e1c
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-10 17:09:33 -0300

    style(page/Timeline): add responsiveness

--- page/src/pages/Home/Timeline/index.jsx
+++ page/src/pages/Home/Timeline/index.jsx
@@ -1,25 +1,36 @@
-import { H1 } from "../../../styles/ui";
-import activities from '../../../datas/datas.json';
+import { useState } from 'react';
+import { H1 } from '../../../styles/ui';
 import { 
-  TimelineWrapper, 
-  TimelineList, 
-  TimelineItem, 
-  ModuleTitle, 
-  ActivityTitle 
+  TimelineWrapper,
+  BootcampListWrapper,
+  BootcampItemWrapper,
+  BootcampItemContent,
+  BootcampModuleTitle,
+  BootcampActivityTitle,
+  ShowMoreBtn
 } from './style';
 
-export function Timeline() {
-  return(
+export function Timeline({ bootcamps }) {
+  const [loadMore, setLoadMore] = useState(3);
+  const loadMoreItems = () => setLoadMore(loadMore + 3);
+
+  return (
     <TimelineWrapper>
-      <H1>Timeline</H1>
-      <TimelineList>
-        {activities.map(activity => (
-          <TimelineItem key={activity.id}>
-            <ModuleTitle>{activity.modulo}</ModuleTitle>
-            <ActivityTitle>{activity.activity}</ActivityTitle>
-          </TimelineItem>
+      <H1>Timeline:</H1>
+      <BootcampListWrapper>
+        {[...bootcamps].reverse().slice(0, loadMore).map(item => (
+        <BootcampItemWrapper key={item.id}>
+          <BootcampItemContent>
+            <BootcampModuleTitle>{item.modulo}</BootcampModuleTitle>
+            <BootcampActivityTitle>{item.activity}</BootcampActivityTitle>
+          </BootcampItemContent>
+        </BootcampItemWrapper>
+
         ))}
-      </TimelineList>
+      </BootcampListWrapper>
+
+      <ShowMoreBtn onClick={loadMoreItems}>carregar mais</ShowMoreBtn>
     </TimelineWrapper>
-  )
+
+  );
 }
--- page/src/pages/Home/Timeline/style.js
+++ page/src/pages/Home/Timeline/style.js
@@ -2,60 +2,146 @@ import styled from 'styled-components';
 import { layoutColors } from '../../../styles/colors';
 
 export const TimelineWrapper = styled.div`
+  width: 100%;
   padding: 2.4rem;
+
   display: flex;
   flex-direction: column;
-  align-items: center;
   gap: 2.4rem;
 `
 
-export const TimelineList = styled.ul`
-  padding-left: 1.6rem;
-  border-left: 4px solid ${layoutColors.highlight};
-
+export const BootcampListWrapper = styled.ul`
+  width: 100%;
   display: flex;
-  flex-direction: column-reverse;
-  gap: 1.8rem;
+  flex-direction: column;
+  gap: 2rem;
 
+  position: relative;
+  
+  &::after {
+    content: '';
+    width: 4px;
+    height: 100%;
+    background-color: ${layoutColors.secondary};
+    border-radius: 8px;
+
+    position: absolute;
+    z-index: -1;
+  }
+  
+  @media screen and (min-width: 700px) {
+    width: 70rem;
+    margin: auto;
+
+    &::after {
+    left: calc(50% + 4px);
+  }
+  }
 `
 
-export const TimelineItem = styled.li`
-position: relative;
+export const BootcampItemWrapper = styled.li`
+  max-width: 40rem;
+  padding: 1.8rem;
+  margin-left: 2rem;
+  
+  border-radius: 8px;
+  background-color: ${layoutColors.shadow};
+  filter: drop-shadow(4px 4px 4px black);
+  display: flex;
+  justify-content: flex-start;
+
+  position: relative;
   transition: all .4s ease;
-  padding: 2rem 0.8rem;
 
-  &::before {
-    content: "";
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
+  &::after {
+    content: '';
+    width: 2rem;
+    height: 2rem;
     background-color: ${layoutColors.secondary};
-    position: absolute;
-    left: -18px;
-    transform: translateX(-50%);
-  }  
+    border-radius: 50%;
 
-  &:last-child::before {
+    position: absolute;
+    top: 50%;
+    left: -2.8rem;
+    z-index: 2;
+    transform: translateY(-50%);
+  }
+  
+  &:first-child::after {
     background-color: ${layoutColors.light1};
   }
-
+  
   &:hover {
     cursor: pointer;
-    background-color: ${layoutColors.shadow};
+    background-color: ${layoutColors.secondary};
     filter: drop-shadow(4px 4px 4px black);
+  } 
+  
+  @media screen and (min-width: 700px) {
+    width: 32.5rem;
+    margin: 0;
+    
+    &:nth-child(odd) {
+      margin-left: 2rem;
+      align-self: flex-end;
+    }
+
+    &:nth-child(even) {
+      margin-right: 2rem;
+      align-self: flex-start;
+      text-align: right;
+
+      &::after {
+        content: '';
+        width: 2rem;
+        height: 2rem;
+        background-color: ${layoutColors.secondary};
+        border-radius: 50%;
+  
+        position: absolute;
+        top: 50%;
+        left: 34.5rem;
+        z-index: 2;
+        transform: translateY(-50%);
+      }
+    }
   }
 `
+export const BootcampItemContent = styled.div`
+  width: 100%;
+`
 
-export const ModuleTitle = styled.h3`
+export const BootcampModuleTitle = styled.h3`
   color: ${layoutColors.tertiary};
   font-size: 2rem;
   font-weight: 700;
 `
 
-export const ActivityTitle = styled.h4`
+export const BootcampActivityTitle = styled.h4`
   margin-top: 1.2rem;
   color: ${layoutColors.text};
   font-size: 1.6rem;
   font-weight: 500;
   line-height: 24px;
 `
+
+export const ShowMoreBtn = styled.button`
+   margin: auto;
+   color: ${layoutColors.hightlight};
+   font-size: 1.8rem;
+   text-transform: uppercase;
+  
+   background-color: ${layoutColors.shadow};
+   border: none;
+   border: 2px solid ${layoutColors.hightlight};
+   border-radius: 8px;
+   padding: .8rem 1.8rem;
+
+   transition: all .4s ease;
+  
+   &:hover {
+     color: ${layoutColors.highlightHover};
+     border: 2px solid ${layoutColors.highlightHover};
+     cursor: pointer;
+   }
+ `
