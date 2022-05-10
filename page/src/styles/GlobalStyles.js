a4a433e87f4d8b6d5c4918c0d624dcc42428c07b
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-10 20:47:53 -0300

    style(page/global): remove useless styles

--- page/src/styles/GlobalStyles.js
+++ page/src/styles/GlobalStyles.js
@@ -14,15 +14,6 @@ export default createGlobalStyle`
     font-size: 62.5%;
 
     background-color: ${layoutColors.primary};
-
-    ${lightBg1(layoutColors.light1, -10, 80)}
-    ${lightBg2(layoutColors.light2, 54, -18)}
-    overflow-x: hidden;
-    
-    @media screen and (min-width: 700px) {
-      ${lightBg1(layoutColors.light1, 10, 80)}
-      ${lightBg2(layoutColors.light2, 54, 8)}
-    }
   }
 
   body {
