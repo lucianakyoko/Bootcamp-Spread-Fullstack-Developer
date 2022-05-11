87f2d548aabc75f8ccffe8ea442be629cdf30dd0
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-11 14:26:14 -0300

    styles(page/Project): add background light

--- page/src/pages/Projects/style.js
+++ page/src/pages/Projects/style.js
@@ -1,19 +1,27 @@
 import styled from 'styled-components';
 import { layoutColors } from "../../styles/colors";
+import { lightBg1, lightBg2 } from '../../styles/mixins';
 
 
 export const ProjectSection = styled.section`
-
+  width: 100%;
+  overflow-x: hidden;
   padding: 2.4rem;
 
   display: flex;
   flex-direction: column;
   gap: 2.8rem;
 
+  ${lightBg1(layoutColors.light1, 10, 80)};
+  ${lightBg2(layoutColors.light2, 60, -40)};
+
   @media screen and (min-width: 700px) {
     padding: 2.4rem 16rem;
     margin: auto;
     gap: 3.2rem;
+
+    ${lightBg1(layoutColors.light1, 10, 80)}
+    ${lightBg2(layoutColors.light2, 60, 10)}
   }
 `
 
