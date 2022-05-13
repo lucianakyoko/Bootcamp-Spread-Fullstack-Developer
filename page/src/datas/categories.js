ec7410ec531f909df142a5d8b3dd7b2f1d91486b
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-13 09:46:23 -0300

    feat(page/datas): create categories file

---
+++ page/src/datas/categories.js
@@ -0,0 +1,16 @@
+const categories = [
+  {
+    "id": "curso",
+    "label": "Curso",
+    "color": "#FE0ABA"
+  },
+  {
+    "id": "desafio",
+    "label": "Desafio",
+    "color": "#118884"
+  },
+]
+
+export const getCategories = () => {
+  return categories;
+};
