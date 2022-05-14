4baa8267cba445e9fff48e2b10234287653a5578
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-05-14 18:59:42 -0300

    docs(MFR/practice): add class 02 practice lesson - Filter

---
+++ MFR - Map Filter Reduce/practice/filter.js
@@ -0,0 +1,11 @@
+/* Filtre e retorne todos os números pares de um array. */
+
+function filtraPares(arr) {
+	if (!arr || !arr.length) return;
+
+	const filteredArr = arr.filter((item) => item % 2 === 0);
+
+	return filteredArr;
+}
+
+console.log(filtraPares([1, 2, 3, 4]));
